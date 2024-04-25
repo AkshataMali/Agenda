dda69a646d3de97b06bd2e8eaa8c3422f198cdf7
AkshataMali <97438089+AkshataMali@users.noreply.github.com>
2024-04-25 23:12:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,37 @@
-*{
-    margin: 0;
-    padding: 0;
-    font-family: 'Gill Sans', sans-serif;
-    box-sizing: border-box;
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Do It ASAP😒");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    inputBox.value = " ";
+    saveData();
 }
 
-.container{
-    width: 100%;
-    min-height: 100vh;
-    background: linear-gradient(135deg , #b679ef ,#4e085f);
-    padding: 10px;
+listcontainer.addEventListener("click", function (e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
 }
 
-.todo-app{
-    width: 100%;
-    max-width: 540px;
-    background: #ffff;
-    margin: 100px auto 20px;
-    padding: 40px 30px 70px;
-    border-radius: 20px;
-    align-items: center;
+function showList(){
+    listcontainer.innerHTML = localStorage.getItem("data");
 }
-
-.todo-app h2{
-    color: black;
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-    align-items: center;
-}
-.todo-app h2 img{
-    width: 40px;
-    margin-left: 10px;
-}
-.row{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background: #edeef0;
-    border-radius: 30px;
-    padding-left: 20px;
-    margin-bottom: 25px;
-}
-input{
-    flex: 1;
-    border: none;
-    outline: none;
-    background: transparent;
-    padding: 10px;
-    font-weight: 14px;
-}
-button{
-    border : none;
-    outline: none;
-    padding: 16px 80px;
-    background: #6a17b7;
-    color: #ffff;
-    font-size: 16px;
-    cursor: pointer;
-    border-radius: 40px;
-}
-ul li{
-    list-style: none;
-    font-size: 17px;
-    padding: 12px 8px 12px 40px;
-    user-select: none;
-    cursor: pointer;
-    position: relative;
-}
-ul li::before{
-    content: ' ' ;
-    position: absolute;
-    height: 28px;
-    width: 28px;
-    border-radius: 50%;
-    background-image: url(images/unchecked.png);
-    background-size: cover;
-    background-position: center;
-    top: 12px;
-    left: 8px;
-}
-ul li.checked{
-    color: #2d2b2b;
-    text-decoration: line-through;
-}
-ul li.checked::before{
-    background-image: url(images/Checked.png);
-}
-ul li span{
-    position: absolute;
-    right: 0;
-    top: 5px;
-    width: 40px;
-    height: 40px;
-    font-size: 22px;
-    color: rgb(19, 17, 17);
-    line-height: 40px;
-    text-align: center;
-    border-radius: 50%;
-}
-ul li span:hover{
-    background: #edeef0;
-} 
+showList();
